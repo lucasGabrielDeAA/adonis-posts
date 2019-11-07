6114ae4e1ccf2d4d9d74e9f4d6b4d8986898ed5e
lucasGabrielDeAA <lg.technus@gmail.com>
2019-11-07 10:46:54 -0300

    PostController added

---
+++ app/Controllers/Http/PostController.js
@@ -0,0 +1,88 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+const Post = use('App/Models/Post');
+
+/**
+ * Resourceful controller for interacting with posts
+ */
+class PostController {
+  /**
+   * Show a list of all posts.
+   * GET posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index ({ request, response, view }) {
+    const posts = await Post.query().with('owner').fetch();
+
+    return posts;
+  }
+
+  /**
+   * Create/save a new post.
+   * POST posts
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, auth, response }) {
+    const data = request.only(['content']);
+    const post = await Post.create({ user_id: auth.user.id, ...data });
+
+    return post;
+  }
+
+  /**
+   * Display a single post.
+   * GET posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+    const post = await Post.findOrFail(params.id);
+
+    return post;
+  }
+
+  /**
+   * Update post details.
+   * PUT or PATCH posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a post with id.
+   * DELETE posts/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, auth, request, response }) {
+    const post = await Post.findOrFail(params.id);
+
+    if (post.user_id !== auth.user.id) {
+      return response.status(401);
+    }
+
+    await post.delete();
+  }
+}
+
+module.exports = PostController
