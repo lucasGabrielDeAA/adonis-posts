828d3ce67f8818e7696048d6e6acaad63f2ee289
lucasGabrielDeAA <lg.technus@gmail.com>
2019-11-07 10:45:11 -0300

    Post's migration added

---
+++ database/migrations/1573132446773_post_schema.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PostSchema extends Schema {
+  up () {
+    this.create('posts', (table) => {
+      table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE')
+      table.string('content', 240).notNullable()
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('posts')
+  }
+}
+
+module.exports = PostSchema
